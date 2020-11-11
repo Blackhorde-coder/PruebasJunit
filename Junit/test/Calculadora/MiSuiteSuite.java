8ea04fa871af97f0bdc981b38717c14626e5006d
Blackhorde <esingcomputacion@gmail.com>
2020-11-11 20:02:11 -0300

    SuitSuit
    Final

---
+++ Junit/test/Calculadora/MiSuiteSuite.java
@@ -0,0 +1,14 @@
+
+package Calculadora;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith (value = Suite.class)
+@Suite.SuiteClasses({
+    CalculadoraTest.class,
+    CalculadoraParametro.class
+})
+public class MiSuiteSuite {
+    
+}
